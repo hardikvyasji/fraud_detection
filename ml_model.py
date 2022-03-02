bb0ceb03d73c60c205a344b31f085291a65aca1a
hardikvyasji <hvhardikvyas@live.in>
2022-03-02 15:01:45 +0530

    added 3 new  ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,14 @@
 
 
 print('ML realted code here')
+
+
+def model1():
+    print('code for model1')
+
+def model2():
+    print('code for model2')
+
+
+def model3():
+    print('code for model3')            
